990b19133686a3d2861415e2a487fd1b9aa8af2c
cokelsy <lsy020379@163.com>
2026-05-06 15:21:15 +0800

    Add main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import CalendarWrapper from '@/components/Calendar/CalendarWrapper';
+
+export default function MainPage() {
+  return (
+    <div className="h-full flex flex-col">
+      <CalendarWrapper />
+    </div>
+  );
+}
